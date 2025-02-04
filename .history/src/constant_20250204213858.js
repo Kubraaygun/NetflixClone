897e9079929df2a3feaa458514143babd6ae2375
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-02-04 21:39:58 +0300

    Update constants

---
+++ .history/src/constant_20250204213858.js
@@ -0,0 +1,12 @@
+export const options = {
+
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNWVlMDBhNmI1NzFiNzY2ZDdjNzJlYjhiMjcyOWExZiIsInN1YiI6IjY1YzhiYzMxNjgwYmU4MDE3ZWE1MzM2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qsGaH3MIAXDO1LdD9sK1mnCaM8Eo0UIl3pxjlC0vVGQ'
+  }
+};
+
+
+
+
+  export const baseImgURL= 'https://image.tmdb.org/t/p/original';
